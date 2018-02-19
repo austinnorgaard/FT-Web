78605597d61ad199fe9a2b580e84612c9c5c16d1
Keaton Freude <freudek90@gmail.com>
2018-02-18 16:32:00 -0800

    Add nestjs smart-cone backend

--- field-trainer/field-trainer/src/app/app.component.ts
+++ field-trainer/field-trainer/src/app/app.component.ts
@@ -2,24 +2,26 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: "ft-app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+    selector: "ft-app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  constructor(private router: Router) {
-    this.router = router;
-  }
+    constructor(private router: Router) {
+        this.router = router;
+    }
 
-  currentPath(): string {
-    // take the router url and extract a pretty
-    // path for our title bar
-    const titleString = this.prettyString(this.router.url.replace("/", " - "));
-    return `FieldTrainer ${titleString}`;
-  }
+    currentPath(): string {
+        // take the router url and extract a pretty
+        // path for our title bar
+        const titleString = this.prettyString(
+            this.router.url.replace("/", " | ")
+        );
+        return `Field Trainer ${titleString}`;
+    }
 
-  private prettyString(item: string) {
-    // Hacky, hard-coded solution.
-    return item.substr(0, 3) + item.charAt(3).toUpperCase() + item.slice(4);
-  }
+    private prettyString(item: string) {
+        // Hacky, hard-coded solution.
+        return item.substr(0, 3) + item.charAt(3).toUpperCase() + item.slice(4);
+    }
 }
