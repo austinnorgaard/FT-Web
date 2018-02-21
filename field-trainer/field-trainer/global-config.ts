5e6fb0ca6aca1cfdb6a14e260222bb307771a781
Keaton Freude <freudek90@gmail.com>
2018-02-20 21:14:27 -0800

    modify IP & port details for global config -- not generically correct

--- field-trainer/field-trainer/global-config.ts
+++ field-trainer/field-trainer/global-config.ts
@@ -1,5 +1,5 @@
 // Set this field to the IP of the start-cone or device hosting the front-end
-export const start_cone_ip = "192.168.1.8";
+export const start_cone_ip = "192.168.1.115";
 
 // The port the front-end website is hosted on
 export const front_end_port = "4200";
@@ -11,7 +11,7 @@ export const smart_cone_api_http_port = "3000";
 export const cone_api_socket_port = "3001";
 
 // The port the smart_cone is hosting the smart_cone_api server on (front-end -> smart_cone)
-export const smart_cone_api_socket_port = "3002";
+export const smart_cone_api_socket_port = "3000";
 
 // The port each cone (except the smart_cone) is hosting their http server on
 export const cone_api_http_port = "3100";
