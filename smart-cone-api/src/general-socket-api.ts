76a06377a385a70d8d60f7f555c249e7b83aa597
Keaton Freude <freudek90@gmail.com>
2018-02-20 21:13:42 -0800

    Add general socket API for general ft->sc comms

---
+++ smart-cone-api/src/general-socket-api.ts
@@ -0,0 +1,24 @@
+import {
+    WebSocketGateway,
+    WsResponse,
+    SubscribeMessage,
+    OnGatewayConnection,
+    OnGatewayDisconnect
+} from "@nestjs/websockets";
+
+@WebSocketGateway()
+export class GeneralSocketApi implements OnGatewayConnection, OnGatewayDisconnect {
+    handleConnection(client: any) {
+        console.log(`Connection received. Data: ${client}`);
+    }
+
+    handleDisconnect(client: any) {
+        console.log(`Disconnection received. Data: ${client}`);
+    }
+
+    @SubscribeMessage("test")
+    onTest(client, data): WsResponse<any> {
+        const event = "test";
+        return { event, data };
+    }
+}
