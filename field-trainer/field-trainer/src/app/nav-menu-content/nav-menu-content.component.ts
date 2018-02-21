9990f54391652a39992ba3c61f5f979f6082c514
Keaton Freude <freudek90@gmail.com>
2018-02-20 22:07:09 -0800

    Close sidenav after selecting something. Add quick links to pages

--- field-trainer/field-trainer/src/app/nav-menu-content/nav-menu-content.component.ts
+++ field-trainer/field-trainer/src/app/nav-menu-content/nav-menu-content.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { MatSidenav } from "@angular/material/sidenav";
 
 @Component({
-  selector: 'ft-nav-menu-content',
-  templateUrl: './nav-menu-content.component.html',
-  styleUrls: ['./nav-menu-content.component.css']
+    selector: "ft-nav-menu-content",
+    templateUrl: "./nav-menu-content.component.html",
+    styleUrls: ["./nav-menu-content.component.css"]
 })
 export class NavMenuContentComponent implements OnInit {
+    @Input() sideNav: MatSidenav;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    close() {
+        this.sideNav.close();
+    }
+
+    open() {
+        this.sideNav.open();
+    }
 }
