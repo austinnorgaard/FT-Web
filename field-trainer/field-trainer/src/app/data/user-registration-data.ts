892a83cb3bf03f8c1a7e3d09795f56474aa4b134
Keaton Freude <freudek90@gmail.com>
2018-02-22 20:41:18 -0800

    Add user registration validation & data binding

---
+++ field-trainer/field-trainer/src/app/data/user-registration-data.ts
@@ -0,0 +1,50 @@
+export class UserRegistrationData {
+    prefix: string;
+    firstName: string;
+    lastName: string;
+    address1: string;
+    address2: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    country: string;
+    phoneNumber: string;
+    emailAddress: string;
+    password: string;
+    subscribeToNewsletter: boolean;
+
+    constructor() {
+        this.prefix = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.address1 = "";
+        this.address2 = "";
+        this.city = "";
+        this.state = "";
+        this.zipCode = "";
+        this.country = "";
+        this.phoneNumber = "";
+        this.emailAddress = "";
+        this.password = "";
+        this.subscribeToNewsletter = false;
+    }
+
+    // returns if this instance is valid (all required fields are selected)
+    isValid(): Boolean {
+        console.log(this.prefix);
+        if (this.prefix === null || this.prefix === "") return false;
+        if (this.firstName === "") return false;
+        if (this.lastName === "") return false;
+        if (this.address1 === "") return false;
+        if (this.address2 === "") return false;
+        if (this.city === "") return false;
+        if (this.state === "") return false;
+        if (this.zipCode === "") return false;
+        if (this.country === "") return false;
+        if (this.phoneNumber === "") return false;
+        if (this.emailAddress === "") return false;
+        if (this.password === "") return false;
+
+        return true;
+    }
+}
--- field-trainer/field-trainer/src/app/register-page/register-page.component.ts
+++ field-trainer/field-trainer/src/app/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { UserRegistrationData } from "../data/user-registration-data";
 
 @Component({
     selector: "ft-app-register-page",
@@ -6,6 +7,8 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./register-page.component.css"]
 })
 export class RegisterPageComponent implements OnInit {
+    userData: UserRegistrationData = new UserRegistrationData();
+
     readonly prefixes: string[] = ["Mr", "Mrs", "Ms"];
 
     readonly countries: string[] = [
@@ -13,10 +16,23 @@ export class RegisterPageComponent implements OnInit {
         "United Kingdom",
         "Australia",
         "New Zealand",
+        "Canada",
+        "Greece",
+        "Kazakhstan",
         "Some other country"
     ];
 
     constructor() {}
 
     ngOnInit() {}
+
+    submit(): void {
+        console.log(this.userData);
+
+        if (this.userData.isValid()) {
+            console.log("Data is valid!");
+        } else {
+            console.log("ERROR: Data invalid!");
+        }
+    }
 }
