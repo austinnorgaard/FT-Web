9781d2a2dd6d3aedb5f15585d4e2c52fd2529aef
Keaton Freude <freudek90@gmail.com>
2018-03-04 14:54:14 -0800

    Add validation to Add Team Component

--- field-trainer/field-trainer/src/app/add-team/add-team.component.ts
+++ field-trainer/field-trainer/src/app/add-team/add-team.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from "@angular/core";
 
+import { FormControl, Validators } from "@angular/forms";
+
+import { AddTeamData } from "../models/add-team-data";
+
 @Component({
     selector: "ft-add-team",
     templateUrl: "./add-team.component.html",
     styleUrls: ["./add-team.component.css"]
 })
-export class AddTeamComponent implements OnInit {
+export class AddTeamComponent {
     ageGroups: string[] = ["11-13", "13-15", "15-17"];
+    genders: string[] = ["Male", "Female"];
+    submitted = false;
+    addTeamData: AddTeamData = new AddTeamData();
+
+    public teamNameFormControl = new FormControl("", [
+        Validators.required,
+        Validators.minLength(3)
+    ]);
+
+    public ageGroupFormControl = new FormControl("", [Validators.required]);
 
     constructor() {}
 
-    ngOnInit() {}
+    onSubmit(): void {
+        if (this.teamNameFormControl.valid === false) {
+            console.log("Error: Team Name not valid.");
+            return;
+        }
 
-    onClick(): void {
-        console.log("Hello, world!");
+        console.log(JSON.stringify(this.addTeamData));
     }
 }
