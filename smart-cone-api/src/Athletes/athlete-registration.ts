ade76bbf494522823e4c9088bd0db1386ad01ef7
Keaton Freude <freudek90@gmail.com>
2018-04-11 11:01:59 -0700

    Upgrade versions, add validation for athlete registration

--- smart-cone-api/src/Athletes/athlete-registration.ts
+++ smart-cone-api/src/Athletes/athlete-registration.ts
@@ -1,8 +1,15 @@
 import { Athlete } from "./athlete";
 import { Team } from "../Teams/Team";
-import { IsOptional } from "class-validator";
+import { IsOptional, ValidateNested, IsDefined } from "class-validator";
+import { Type } from "class-transformer";
 
 export abstract class AthleteRegistration {
+    @ValidateNested()
+    @Type(() => Athlete)
+    @IsDefined()
     athlete: Athlete;
+
+    @ValidateNested()
+    @Type(() => Team)
     @IsOptional() team: Team;
 }
