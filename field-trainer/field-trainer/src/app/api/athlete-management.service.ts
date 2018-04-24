7c5b4fc5fb63c1e5dffeb6d9fa94bafb54e0d131
Keaton Freude <freudek90@gmail.com>
2018-04-23 21:28:37 -0700

    Add getAthletes service method

--- field-trainer/field-trainer/src/app/api/athlete-management.service.ts
+++ field-trainer/field-trainer/src/app/api/athlete-management.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { AthleteRegistration } from "../../../../../smart-cone-api/src/Athletes/athlete-registration";
 import { DatabaseResponse } from "../../../../../smart-cone-api/src/Database/Data/DatabaseResponse";
 import { FT_CONFIG } from "../../../global-config";
+import { Athlete } from "../../../../../smart-cone-api/src/Athletes/athlete";
 
 @Injectable()
 export class AthleteManagementService {
@@ -13,5 +14,12 @@ export class AthleteManagementService {
             .post<DatabaseResponse>(FT_CONFIG.toSmartConeHttp("/athletes"), athleteRegistration)
             .toPromise()
             .catch(err => Promise.reject(err as DatabaseResponse));
+    }
+
+    getAthletes(): Promise<Athlete[]> {
+        return this.http
+            .get<Athlete[]>(FT_CONFIG.toSmartConeHttp("/athletes"))
+            .toPromise()
+            .catch(err => Promise.reject(err));
     }
 }
