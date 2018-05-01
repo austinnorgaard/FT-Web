a63b2bf74c848c8491b6337cc908f36e61befe01
Keaton Freude <freudek90@gmail.com>
2018-04-30 18:47:27 -0700

    Add a script to run prettier across source folders

---
+++ run_prettier.sh
@@ -0,0 +1,16 @@
+# Runs Prettier (a code formatter) across the whole project
+# We will selectively choose source folders so we can avoid
+# formatting unnecessary folders
+
+directories=("./smart-cone-api/src" "./field-trainer/field-trainer/src")
+config="./.prettierrc"
+echo Using config located at $config
+for i in "${directories[@]}"
+do
+    :
+    echo Prettifying $i
+    # run prettier on this file, using default config specified above
+    args="--config=${config} --write ${i}/**/*.ts"
+    echo Using args: $args
+    prettier $args
+done
