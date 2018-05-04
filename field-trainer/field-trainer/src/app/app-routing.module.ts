694bdb4530014d3ddc7051d54810afa11d16d9e6
Keaton Freude <freudek90@gmail.com>
2018-05-03 18:38:01 -0700

    Add material typography to app. start consolidating header styles

--- field-trainer/field-trainer/src/app/app-routing.module.ts
+++ field-trainer/field-trainer/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomePageComponent } from "./home-page/home-page.component";
 import { RegisterPageComponent } from "./register-page/register-page.component";
 import { AddTeamComponent } from "./add-team/add-team.component";
 import { AuthGuardService as AuthGuard } from "./_services/auth-guard.service";
-import { AthleteManagementComponent } from "./athlete-management/athlete-management.component";
+import { AthleteManagementPageComponent } from "./athlete-management-page/athlete-management-page.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { TeamManagementPageComponent } from "./team-management-page/team-management-page.component";
 
@@ -24,7 +24,7 @@ const routes: Routes = [
     },
     {
         path: "athlete-management",
-        component: AthleteManagementComponent,
+        component: AthleteManagementPageComponent,
         canActivate: [AuthGuard],
     },
     {
