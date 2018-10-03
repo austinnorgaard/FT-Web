04abee7aa814216e201d6227f1cf2fb32f5fb98c
Keaton Freude <freudek90@gmail.com>
2018-10-02 18:42:01 -0700

    Add setFieldAndCourse to SessionService

--- field-trainer/field-trainer/src/app/api/session.service.ts
+++ field-trainer/field-trainer/src/app/api/session.service.ts
@@ -4,12 +4,26 @@ import { Cone } from "../models/cone";
 import { PlayerSession } from "../models/player-session";
 
 import "rxjs/add/operator/toPromise";
+import { Course } from "../models/course";
+import { Field } from "../models/field";
 
 @Injectable()
 export class SessionService {
+    private selectedCourse: Course;
+    private selectedField: Field;
+
     constructor(private http: Http) {}
 
     setState(sessions: PlayerSession[]) {}
+
+    /*
+     * Sets the currently selected field and course, from when the user is about
+     * to start a session.
+     */
+    setFieldAndCourse(field: Field, course: Course) {
+        this.selectedField = field;
+        this.selectedCourse = course;
+    }
 
     private handleError(error: any): Promise<any> {
         console.error("An error occurred", error);
