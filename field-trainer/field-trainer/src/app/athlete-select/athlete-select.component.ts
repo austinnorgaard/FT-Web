792bccd0c465617640d148b2a737e11f12320b51
Keaton Freude <freudek90@gmail.com>
2018-10-06 12:36:51 -0700

    Add team selection to athlete-select

--- field-trainer/field-trainer/src/app/athlete-select/athlete-select.component.ts
+++ field-trainer/field-trainer/src/app/athlete-select/athlete-select.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { TeamModel } from "../models/team";
+import { TeamManagementService } from "../api/team-management.service";
+import { AthleteManagementService } from "../api/athlete-management.service";
 
 @Component({
     selector: "ft-athlete-select",
@@ -7,11 +10,30 @@ import { Router } from "@angular/router";
     styleUrls: ["./athlete-select.component.css"],
 })
 export class AthleteSelectComponent implements OnInit {
-    constructor(private router: Router) {}
+    protected selectedTeam: TeamModel;
+    protected availableTeams: TeamModel[];
+    constructor(private router: Router, private teamsService: TeamManagementService, private athletesService: AthleteManagementService) {
+        this.loadAvailableTeams();
+    }
 
     ngOnInit() {}
 
     onClick() {
         this.router.navigateByUrl("/session-setup/field-select");
+    }
+
+    onTeamChanged() {
+        console.log("Team changed!");
+    }
+
+    loadAvailableTeams() {
+        this.teamsService
+            .getTeams()
+            .then(teams => {
+                this.availableTeams = teams;
+            })
+            .catch(err => {
+                console.log("Athlete-select: Could not load available teams..");
+            });
     }
 }
