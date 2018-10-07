e6578ebd5c45e2efb887991cc0d360cf082b8727
Keaton Freude <freudek90@gmail.com>
2018-10-06 19:15:04 -0700

    Move athletes up and down

--- field-trainer/field-trainer/src/app/athlete-select/athlete-select.component.ts
+++ field-trainer/field-trainer/src/app/athlete-select/athlete-select.component.ts
@@ -4,6 +4,7 @@ import { TeamModel } from "../models/team";
 import { TeamManagementService } from "../api/team-management.service";
 import { AthleteManagementService } from "../api/athlete-management.service";
 import { AthleteModel } from "../models/athlete";
+import { Athlete } from "../../../../../smart-cone-api/src/Athletes/athlete";
 
 @Component({
     selector: "ft-athlete-select",
@@ -40,13 +41,48 @@ export class AthleteSelectComponent implements OnInit {
 
     onMoveAthleteUp(athlete: AthleteModel) {
         console.log("Move-up: ", athlete);
+        // Move this athlete up (closer to the front of the array)
+        // First, check if we even need to do anything..
+
+        const index = this.getIndexOfAthlete(athlete);
+
+        if (index === 0) {
+            return;
+        }
+
+        // Ok, so the athlete can be moved up, so swap the elements at index and index -1
+        this.swapAthlete(index, index - 1);
     }
 
     onMoveAthleteDown(athlete: AthleteModel) {
         console.log("Move-down: ", athlete);
+        const index = this.getIndexOfAthlete(athlete);
+
+        if (index === this.selectedTeam.teamAthletes.length - 1) {
+            return;
+        }
+
+        // Ok, so the athlete can be moved down, so swap the elements at index and index +1
+        this.swapAthlete(index, index + 1);
     }
 
     onSetAthleteInactive(athlete: AthleteModel) {
         console.log("Set-Inactive: ", athlete);
+    }
+
+    // Swaps the athlete at position 'x' with the athlete at position 'y'
+    // from the currently selected team
+    swapAthlete(x: number, y: number) {
+        // This some weird looking javascript, but it looks cool anyway
+        [this.selectedTeam.teamAthletes[x], this.selectedTeam.teamAthletes[y]] = [
+            this.selectedTeam.teamAthletes[y],
+            this.selectedTeam.teamAthletes[x],
+        ];
+    }
+
+    getIndexOfAthlete(athlete: Athlete) {
+        return this.selectedTeam.teamAthletes.findIndex(
+            a => a.firstName === athlete.firstName && a.lastName === athlete.lastName && a.email === athlete.email,
+        );
     }
 }
