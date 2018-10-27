c75b982bbef846cfc9fb920b214465ff17befe91
Keaton Freude <freudek90@gmail.com>
2018-10-26 21:34:08 -0700

    Change default IP for the FT Config

--- deployment/update-smart-cone.sh
+++ deployment/update-smart-cone.sh
@@ -8,10 +8,14 @@ cd ~/FT-WEB && git checkout master && git pull
 # We have the latest now, rebuild the front-end and backend
 cd ~/FT-WEB/field-trainer/field-trainer && npm install
 cd ~/FT-WEB/smart-cone-api/ && npm install
+cd ~/FT-WEB/serve-frontend/ && npm install
 
 # Build the frontend and deploy it
 
 cd ~/FT-WEB/field-trainer/field-trainer && npm run build && mv ./dist ../../serve-frontend
+
+# Build backend
+cd ~/FT-WEB/smart-cone-api/ && npm run prestart:prod
 
 # Restart the frontend & backend services
 sudo /etc/init.d/smartcone stop
