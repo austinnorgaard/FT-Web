4997305acfd01e18fcf1b129f38b1d04027135d6
Keaton Freude <freudek90@gmail.com>
2018-11-13 20:34:55 -0800

    Improve update script

--- deployment/update-smart-cone.sh
+++ deployment/update-smart-cone.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# For now... recopy any file which may change in the update script
+# TODO: Need to split file updates away from bootstrap content...
+sudo cp ~/FT-WEB/deployment/smartcone /etc/init.d/smartcone
+sudo chmod +x /etc/init.d/smartcone
+sudo update-rc.d smartcone defaults
+
 # This needs to be run when the cone has an internet connection, otherwise
 # it will not be able to pull in new releases from GitHub
+echo "Disabling Frontend and Backend services"
+
+sudo service smartcone stop
 
 cd ~/FT-WEB && git checkout master && git pull
 
@@ -12,11 +21,10 @@ cd ~/FT-WEB/serve-frontend/ && env JOBS=4 npm install
 
 # Build the frontend and deploy it
 
-cd ~/FT-WEB/field-trainer/field-trainer && npm run build && mv ./dist ../../serve-frontend
+cd ~/FT-WEB/field-trainer/field-trainer && npm run build && sudo rm -rf ~/FT-WEB/serve-frontend/dist && mv ~/FT-WEB/field-trainer/field-trainer/dist ~/FT-WEB/serve-frontend
 
 # Build backend
 cd ~/FT-WEB/smart-cone-api/ && npm run prestart:prod
 
 # Restart the frontend & backend services
-sudo /etc/init.d/smartcone stop
-sudo /etc/init.d/smartcone start
+sudo service smartcone start
