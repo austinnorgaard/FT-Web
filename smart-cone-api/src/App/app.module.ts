2cfc75a5de646b93bb9ba504677ed363dc7c3464
Keaton Freude <freudek90@gmail.com>
2018-12-29 20:26:53 -0800

    Add logging module

--- smart-cone-api/src/App/app.module.ts
+++ smart-cone-api/src/App/app.module.ts
@@ -12,9 +12,10 @@ import { TeamsModule } from "../Teams/teams.module";
 import { UsersModule } from "../Users/users.module";
 import { AthletesModule } from "../Athletes/athletes.module";
 import { TrainingModule } from "../Training/training.module";
+import { LoggingModule } from "../Logging/logging.module";
 
 @Module({
-    imports: [DatabaseModule, AuthModule, TeamsModule, UsersModule, AthletesModule, TrainingModule],
+    imports: [DatabaseModule, AuthModule, TeamsModule, UsersModule, AthletesModule, TrainingModule, LoggingModule],
     controllers: [AppController, StatusController],
     components: [GeneralSocketApi],
     modules: [DatabaseModule, AuthModule, TeamsModule, UsersModule, AthletesModule],
