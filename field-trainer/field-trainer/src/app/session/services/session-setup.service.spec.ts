d65f6383046d30038f7e79908e34fd53c07248c5
Keaton Freude <freudek90@gmail.com>
2019-01-06 22:16:11 -0800

    Add tests for session setup service

--- field-trainer/field-trainer/src/app/session/services/session-setup.service.spec.ts
+++ field-trainer/field-trainer/src/app/session/services/session-setup.service.spec.ts
@@ -1,16 +1,39 @@
+import { ISessionSetupService } from "./session-setup-interface.service";
+import { Field } from "../models/field";
+import { SessionSetupService } from "./session-setup.service";
+import { Course } from "../models/course";
+import { Athlete } from "../../../../../../smart-cone-api/src/Athletes/athlete";
+
 describe("SessionSetupService", () => {
+    let service: ISessionSetupService = null;
     beforeEach(() => {
-        console.log("Starting a test!");
+        // Create our test instance
+        service = new SessionSetupService();
     });
-    it("should pass", () => {
-        const a = true;
-        console.log("hello");
-        expect(a).toBeTruthy();
+
+    it("should be able to set field", () => {
+        const f = new Field([], 20, 24, "test field");
+        service.setField(f);
+        expect(service.getSessionSetupData().field.name === "test field").toBeTruthy();
+        expect(service.getSessionSetupData().field.height === 24 && service.getSessionSetupData().field.width === 20).toBeTruthy();
     });
-    afterEach(() => {
-        console.log("test done!");
+
+    it("should be able to set a course", () => {
+        const f = new Field([], 20, 24, "test field");
+        const c = new Course(f, [], [], "Test Course");
+        service.setCourse(c);
+        expect(service.getSessionSetupData().course.field === f).toBeTruthy();
+        expect(service.getSessionSetupData().course.name === "Test Course");
     });
-    afterAll(() => {
-        console.log("Everythings done!");
+
+    it("should be able to set athletes", () => {
+        const athletes: Athlete[] = [
+            { firstName: "Keaton", lastName: "Freude", phoneNumber: "5555555555" } as Athlete,
+            { firstName: "Doug", lastName: "Flowers", phoneNumber: "4555555555" } as Athlete,
+        ];
+
+        service.setAthletes(athletes);
+        expect(service.getSessionSetupData().athletes.length).toEqual(2);
+        expect(service.getSessionSetupData().athletes).toEqual(athletes);
     });
 });
--- field-trainer/field-trainer/src/app/session/services/session-setup.service.ts
+++ field-trainer/field-trainer/src/app/session/services/session-setup.service.ts
@@ -1,5 +1,29 @@
 import { Injectable } from "@angular/core";
+import { ISessionSetupService } from "./session-setup-interface.service";
 import { Field } from "../models/field";
+import { Course } from "../models/course";
+import { SessionSetupData } from "../models/session-setup-data";
+import { Athlete } from "../../../../../../smart-cone-api/src/Athletes/athlete";
 
 @Injectable()
-export class SessionSetupService {}
+export class SessionSetupService implements ISessionSetupService {
+    private field: Field;
+    private course: Course;
+    private athletes: Athlete[];
+
+    constructor() {}
+
+    setField(field: Field): void {
+        this.field = field;
+    }
+
+    setCourse(course: Course): void {
+        this.course = course;
+    }
+    setAthletes(athletes: Athlete[]): void {
+        this.athletes = Object.assign(athletes);
+    }
+    getSessionSetupData(): SessionSetupData {
+        return { field: this.field, course: this.course, athletes: this.athletes } as SessionSetupData;
+    }
+}
