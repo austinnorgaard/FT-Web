f08b2a6be900dd5030c2fe38d9cce37b8cfc4bae
Keaton Freude <freudek90@gmail.com>
2019-01-14 21:24:28 -0800

    fix import path...

--- field-cone-api/src/app.module.ts
+++ field-cone-api/src/app.module.ts
@@ -5,7 +5,7 @@ import { CommsModule } from "./Comms/comms.module";
 
 import * as util from "util";
 import * as fs from "fs";
-import { getFieldConeId } from "utils/environment-helper";
+import { getFieldConeId } from "./utils/environment-helper";
 import { environment } from "../../field-trainer/field-trainer/src/environments/environment";
 
 const readFile = util.promisify(fs.readFile);
