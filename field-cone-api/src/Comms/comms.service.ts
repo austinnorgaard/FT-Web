99b43cfe3bc33a663ee3206d0c5407be552ce793
Keaton Freude <freudek90@gmail.com>
2019-01-14 20:25:06 -0800

    Add the ability to get cone id automatically

--- field-cone-api/src/Comms/comms.service.ts
+++ field-cone-api/src/Comms/comms.service.ts
@@ -4,23 +4,27 @@ import * as os from "os";
 import { environment } from "../../../field-trainer/field-trainer/src/environments/environment";
 import { FieldConeInfo } from "../../../smart-cone-api/src/FieldCones/field-cone-info";
 
+import * as fs from "fs";
+import * as util from "util";
+
+const readFile = util.promisify(fs.readFile);
+
 @Injectable()
 export class CommsService {
     private socket: SocketIOClient.Socket = null;
     constructor() {
         // Create our socket to the smart cone
         this.socket = io(environment.config.getConeApiSocketUrl());
-        this.socket.on("connect", () => {
+        this.socket.on("connect", async () => {
             console.log("Connected to the smart cone!!");
-            const interfaces = os.networkInterfaces();
-            console.log(interfaces);
-            this.socket.emit("initialContact", this.getFieldConeInfo());
+            const coneInfo = await this.getFieldConeInfo();
+            this.socket.emit("initialContact", coneInfo);
         });
     }
 
-    getFieldConeInfo(): FieldConeInfo {
+    async getFieldConeInfo(): Promise<FieldConeInfo> {
         const interfaces = os.networkInterfaces();
-        const info: FieldConeInfo = { ip: "unknown", id: 0 };
+        const info: FieldConeInfo = { ip: "unknown", id: -1 };
 
         if (Object.keys(interfaces).some(k => k === "Wi-Fi")) {
             console.log("We are on windows!");
@@ -30,6 +34,19 @@ export class CommsService {
             info.ip = interfaces.wlan0.filter(i => i.family === "IPv4")[0].address;
         }
 
+        const coneId = await this.getFieldConeId();
+        info.id = coneId;
         return info;
+    }
+
+    async getFieldConeId(): Promise<number> {
+        // The field-cone ID is stored at /var/tmp/.cone-id if the device has been bootstrapped
+        // otherwise, we can just return -1 as an obvious indicator of an invalid ID
+        try {
+            const data = await readFile("/var/tmp/.cone-id");
+            return parseInt(data.toString("ascii"), 10);
+        } catch (err) {
+            return -1;
+        }
     }
 }
