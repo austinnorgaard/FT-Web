276fd211f90d2d7771eae27a9328c62ff9e325b3
Keaton Freude <freudek90@gmail.com>
2019-01-24 20:17:54 -0800

    Get athlete session details working

--- field-trainer/field-trainer/src/app/session/components/segment-details/segment-details.component.ts
+++ field-trainer/field-trainer/src/app/session/components/segment-details/segment-details.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Segment } from "@SmartCone/Training/segment";
 
 @Component({
-  selector: 'ft-segment-details',
-  templateUrl: './segment-details.component.html',
-  styleUrls: ['./segment-details.component.css']
+    selector: "ft-segment-details",
+    templateUrl: "./segment-details.component.html",
+    styleUrls: ["./segment-details.component.css"],
 })
 export class SegmentDetailsComponent implements OnInit {
+    @Input() public segment: Segment;
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
+    getSegmentCompletionTime(): string {
+        //const timeDiff = (this.segment.endTime.getTime() - this.segment.startTime.getTime()) / 1000;
 
+        //return `${timeDiff} s`;
+        return "";
+    }
 }
--- field-trainer/field-trainer/src/app/session/components/session-details-page/session-details-page.component.ts
+++ field-trainer/field-trainer/src/app/session/components/session-details-page/session-details-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { SessionService } from "../../services/session.service";
-import { Segment } from "sc/Training/segment";
+import { Segment } from "@SmartCone/Training/segment";
+import { AthleteSession } from "@SmartCone/Training/athlete-session";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "ft-session-details-page",
@@ -8,9 +10,27 @@ import { Segment } from "sc/Training/segment";
     styleUrls: ["./session-details-page.component.css"],
 })
 export class SessionDetailsPageComponent implements OnInit {
-    segments: Segment[];
-    constructor(private readonly sessionService: SessionService) {
+    public athleteSession: AthleteSession = null;
+    private athleteId = -1;
+
+    constructor(private readonly sessionService: SessionService, private router: Router) {
         console.log("SessionDetailsPageComponent!!");
+
+        const splitRoute = this.router.url.split("/");
+
+        // last one
+        this.athleteId = parseInt(splitRoute[splitRoute.length - 1], 10);
+
+        this.sessionService.getAthleteSessionsObservable().subscribe(sessions => {
+            // only care about the sessions for the athlete on this page
+            this.athleteSession = sessions.find(s => s.athlete.id === this.athleteId);
+
+            if (this.athleteSession === undefined) {
+                // freak out, this shouldn't happen (afaik)
+                console.log(`Session Details Page couldnt find the athlete for this page... ${this.athleteId}`);
+                return;
+            }
+        });
     }
 
     ngOnInit() {}
