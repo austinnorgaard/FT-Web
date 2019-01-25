317abea54118aa61387abde01684cfa9cc1e9753
Keaton Freude <freudek90@gmail.com>
2019-01-24 19:11:20 -0800

    Add relative paths to field-trainer

--- field-trainer/field-trainer/src/app/session/components/session-details-page/session-details-page.component.ts
+++ field-trainer/field-trainer/src/app/session/components/session-details-page/session-details-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { SessionService } from "../../services/session.service";
+import { Segment } from "sc/Training/segment";
 
 @Component({
     selector: "ft-session-details-page",
@@ -7,6 +8,7 @@ import { SessionService } from "../../services/session.service";
     styleUrls: ["./session-details-page.component.css"],
 })
 export class SessionDetailsPageComponent implements OnInit {
+    segments: Segment[];
     constructor(private readonly sessionService: SessionService) {
         console.log("SessionDetailsPageComponent!!");
     }
