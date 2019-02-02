06bbe8054b149a96139854623e19e426239a9434
Keaton Freude <freudek90@gmail.com>
2019-02-01 21:14:57 -0800

    fix deployment issues

--- field-trainer/field-trainer/src/app/socket-message-broker/socket-message-broker.module.ts
+++ field-trainer/field-trainer/src/app/socket-message-broker/socket-message-broker.module.ts
@@ -3,7 +3,7 @@ import { SocketMessageBrokerService } from "./socket-message-broker.service";
 import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
 
 const config: SocketIoConfig = {
-    url: "127.0.0.1:5000",
+    url: "192.168.42.1:5000",
     options: {},
 };
 
