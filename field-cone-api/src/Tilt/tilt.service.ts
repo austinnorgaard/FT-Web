b3eeb8f69bf146fbb3420326aa25c0a65782b3cd
Keaton Freude <freudek90@gmail.com>
2019-02-09 13:03:25 -0800

    add rate limiter to tilt

--- field-cone-api/src/Tilt/tilt.service.ts
+++ field-cone-api/src/Tilt/tilt.service.ts
@@ -6,15 +6,24 @@ import * as fs from "fs";
 
 @Injectable()
 export class TiltService extends BaseTiltService {
+    rateLimited: boolean = false;
     constructor() {
         super();
         console.log("Using the Real Tilt Service");
 
         // do gpio setup
-        gpio.setup(this.getTiltPin());
+        gpio.setup(this.getTiltPin(), gpio.DIR_IN, gpio.EDGE_FALLING);
         gpio.on("change", (channel, value) => {
             console.log("Channel " + channel + " value is now " + value);
-            this.TiltOccured.next();
+            if (!this.rateLimited) {
+                this.TiltOccured.next();
+                this.rateLimited = true;
+                setTimeout(() => {
+                    this.rateLimited = false;
+                }, 5000);
+            } else {
+                // do nothing
+            }
         });
     }
 
