c342652e65173539463d73a2508389e2c4f49175
Keaton Freude <freudek90@gmail.com>
2019-02-09 21:15:15 +0000

    fix tilt sensitivity

--- field-cone-api/src/Tilt/tilt.service.ts
+++ field-cone-api/src/Tilt/tilt.service.ts
@@ -12,9 +12,9 @@ export class TiltService extends BaseTiltService {
         console.log("Using the Real Tilt Service");
 
         // do gpio setup
-        gpio.setup(this.getTiltPin(), gpio.DIR_IN, gpio.EDGE_FALLING);
+        gpio.setup(this.getTiltPin(), gpio.DIR_IN, gpio.EDGE_RISING);
         gpio.on("change", (channel, value) => {
-            console.log("Channel " + channel + " value is now " + value);
+            // console.log("Channel " + channel + " value is now " + value);
             if (!this.rateLimited) {
                 this.TiltOccured.next();
                 this.rateLimited = true;
