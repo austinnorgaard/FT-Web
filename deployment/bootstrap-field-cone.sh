33bfa3711145ee3a64cab4736940ae6faa89db13
Keaton Freude <freudek90@gmail.com>
2019-05-06 17:57:32 -0700

    Update fieldcone to use armv6 node

--- deployment/bootstrap-field-cone.sh
+++ deployment/bootstrap-field-cone.sh
@@ -2,6 +2,13 @@
 
 # This script handles all deployment details for a Field Cone
 
+echo "Installing NodeJS"
+wget https://nodejs.org/dist/v8.16.0/node-v8.16.0-linux-armv6l.tar.gz
+tar xf node-v8.16.0-linux-armv6l.tar.gz
+cd node-v8.16.0-linux-armv6l/ && sudo cp -R * /usr/local/
+echo "NodeJS version: "
+node -v
+
 # install the init.d script
 sudo cp ./fieldcone /etc/init.d/fieldcone
 sudo chmod +x /etc/init.d/fieldcone
