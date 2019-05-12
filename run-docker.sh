3f1a0076292c34c1752c519c21fc5e93e88ab95c
Keaton Freude <freudek90@gmail.com>
2019-05-11 20:44:59 -0700

    Test

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 uname -a
+apt-get install qemu-user-static
 docker pull freudek90/fieldtrainer-build:1.3
 #docker rm ft-build
 docker run --name ft-build freudek90/fieldtrainer-build:1.3
