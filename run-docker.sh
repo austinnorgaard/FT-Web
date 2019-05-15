3d49d0a45c98232981fa00ee839069bcac2a8edb
Keaton Freude <freudek90@gmail.com>
2019-05-14 19:57:41 -0700

    Clean up docker script

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-uname -a
-apt-get update -y && apt-get install -y qemu-user-static
-docker pull freudek90/fieldtrainer-build:1.4
-#docker rm ft-build
-docker run --name ft-build freudek90/fieldtrainer-build:1.4 bash /root/FT-WEB/init-build.sh
+sudo docker pull freudek90/fieldtrainer-build:1.4
+sudo docker rm ft-build
+sudo docker run --name ft-build freudek90/fieldtrainer-build:1.4 bash /root/FT-WEB/init-build.sh
