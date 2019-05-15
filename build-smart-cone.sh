cad12e44a0caf9defe1249b642ec17c6800bb15a
Keaton Freude <freudek90@gmail.com>
2019-05-14 22:30:24 -0700

    temp fixes

--- build-smart-cone.sh
+++ build-smart-cone.sh
@@ -11,7 +11,7 @@ source $NVM_DIR/nvm.sh && nvm install $NODE_VERSION && nvm alias default $NODE_V
 node -v
 npm -v
 
-apt-get install -y pigz
+apt-get install -y pigz parallel
 
 pushd .
 parallel sh -c ::: "cd /root/FT-WEB/smart-cone-api/ && npm install --node_sqlite3_binary_host_mirror=https://s3-us-west-2.amazonaws.com/sqlite3-builds/sqlite3-builds" "cd /root/FT-WEB/field-trainer/field-trainer/ && SASS_BINARY_SITE=https://s3-us-west-2.amazonaws.com/sqlite3-builds/node-bass-build npm install" "cd /root/FT-WEB/serve-frontend && npm install"
@@ -40,7 +40,7 @@ OUTPUT_NAME="smart-cone-package-$(git rev-list --count HEAD).tar.gz"
 
 tar -cf $OUTPUT_NAME -I pigz ./smart-cone-package
 
-aws s3 cp ./$OUTPUT_NAME s3://field-trainer-builds
+/root/.local/bin/aws s3 cp ./$OUTPUT_NAME s3://field-trainer-builds
 
 # stop the instance to save $$$$
-aws ec2 stop-instances --instance-ids i-0ccc4ab7e15faa5d5 --region us-west-2
+/root/.local/bin/aws ec2 stop-instances --instance-ids i-0ccc4ab7e15faa5d5 --region us-west-2
