354b190af628a71396c710596fe9b2cc4582a04d
Keaton Freude <freudek90@gmail.com>
2019-06-22 21:19:42 -0700

    Enable compression when serving site

--- serve-frontend/index.js
+++ serve-frontend/index.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var console = require("console");
+var compression = require("compression");
 var app = express();
 var path = require("path");
 var cors = require("cors");
 var public = path.join(__dirname, "dist");
 
 app.use(cors());
-
+app.use(new compression());
 app.use(express.static(__dirname + "/dist"));
 
 app.all("/*", function(req, res, next) {
