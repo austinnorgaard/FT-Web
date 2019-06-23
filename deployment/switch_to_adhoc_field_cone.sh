23653cb8eefa1844cdafc70a4808d12a8392c7f0
Keaton Freude <freudek90@gmail.com>
2019-06-22 23:55:13 -0700

    Fix a dhoc scripts for fieldcone

--- deployment/switch_to_adhoc_field_cone.sh
+++ deployment/switch_to_adhoc_field_cone.sh
@@ -3,12 +3,12 @@
 # dhcpcd.conf
 # Grab the field cone adhoc dhcpcd.conf file from the deployment folder and replace the existing
 sudo rm /etc/dhcpcd.conf
-sudo cp /home/pi/FT-WEB/deployment/dhcpcd_field_cone_adhoc.conf /etc/dhcpcd.conf
+sudo cp /var/tmp/dhcpcd_field_cone_adhoc.conf /etc/dhcpcd.conf
 sudo chmod 644 /etc/dhcpcd.conf
 
 # rc.local
 sudo rm /etc/rc.local
-sudo cp /home/pi/FT-WEB/deployment/rc.local_adhoc_field_cone /etc/rc.local
+sudo cp /var/tmp/rc.local_adhoc_field_cone /etc/rc.local
 sudo chmod 755 /etc/rc.local
 
 echo "Settings changed, rebooting to apply changes..."
