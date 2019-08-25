8f5116ee1b9d92a16c331fdbe5bcd1e62ee3caca
Keaton Freude <freudek90@gmail.com>
2019-08-25 15:39:19 -0700

    Revert "Upgrade to angular 8 seems to be working"
    This reverts commit 0fdb84ac488c977b2d8aa613ef753a66529f310f.

--- field-trainer/field-trainer/src/app/app.module.ts
+++ field-trainer/field-trainer/src/app/app.module.ts
@@ -35,7 +35,7 @@ export function tokenGetter() {
 @NgModule({
     declarations: [AppComponent, YesNoDialogComponent],
     imports: [
-        NgbModule,
+        NgbModule.forRoot(),
         BrowserModule,
         FlexLayoutModule,
         BrowserAnimationsModule,
