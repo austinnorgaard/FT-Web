7bbe23cae2d0c7106b0c43121cc84ba5a964cc93
Keaton Freude <freudek90@gmail.com>
2019-10-13 19:42:53 -0700

    In progress dev builds

--- packaging/init-build.sh
+++ packaging/init-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup the repo
-cd ~/FT-WEB && git clean -fd && git checkout -- . && git checkout release && git pull
+cd ~/FT-WEB && git clean -fd && git checkout -- . && git checkout $2 && git pull
 
 # Run the real script based on the passed in arg
 echo "Running build script with name build-$1.sh"
@@ -9,15 +9,15 @@ echo "Running build script with name build-$1.sh"
 case $1 in
 
 	smart-cone)
-		cd ~/FT-WEB && bash packaging/smart-cone/build-smart-cone.sh
+		cd ~/FT-WEB && bash packaging/smart-cone/build-smart-cone.sh $3
 		;;
 	
 	field-cone)
-		cd ~/FT-WEB && bash packaging/field-cone/build-field-cone.sh
+		cd ~/FT-WEB && bash packaging/field-cone/build-field-cone.sh $3
 		;;
 	
 	smart-cone-system)
-		cd ~/FT-WEB && bash packaging/smart-cone-system/build-smart-cone-system.sh
+		cd ~/FT-WEB && bash packaging/smart-cone-system/build-smart-cone-system.sh $3
 		;;
 
 	*)
--- packaging/run-docker.sh
+++ packaging/run-docker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Args:"
+echo $1
+echo $2
+echo $3
 sudo docker pull freudek90/fieldtrainer-build:1.8
 sudo docker rm ft-build
-sudo docker run --name ft-build freudek90/fieldtrainer-build:1.8 sh -c "cd /root/FT-WEB && git checkout release && git pull && bash /root/FT-WEB/packaging/init-build.sh $1"
+sudo docker run --name ft-build freudek90/fieldtrainer-build:1.8 sh -c "cd /root/FT-WEB && git checkout $2 && git pull && bash /root/FT-WEB/packaging/init-build.sh $1 $2 $3"
