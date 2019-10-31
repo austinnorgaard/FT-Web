fb1d5cc7245b00448661f2f884af65261a1f9d27
Keaton Freude <freudek90@gmail.com>
2019-10-31 16:15:42 -0700

    Revert "Update docker field-trainer version to 1.9"
    This reverts commit acd3cc74b3edc969522b9ac77789c5b6958541df.

--- packaging/run-docker.sh
+++ packaging/run-docker.sh
@@ -3,6 +3,6 @@ echo "Args:"
 echo $1
 echo $2
 echo $3
-sudo docker pull freudek90/fieldtrainer-build:1.9
+sudo docker pull freudek90/fieldtrainer-build:1.8
 sudo docker rm ft-build
-sudo docker run --name ft-build freudek90/fieldtrainer-build:1.9 sh -c "cd /root/FT-WEB && git checkout $2 && git pull && bash /root/FT-WEB/packaging/init-build.sh $1 $2 $3"
+sudo docker run --name ft-build freudek90/fieldtrainer-build:1.8 sh -c "cd /root/FT-WEB && git checkout $2 && git pull && bash /root/FT-WEB/packaging/init-build.sh $1 $2 $3"
