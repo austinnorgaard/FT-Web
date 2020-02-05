6da8418f7b6b9d114ae56733a8b116367dc8b4f5
Keaton Freude <freudek90@gmail.com>
2020-02-05 13:33:46 -0800

    Enable new audio files on smart cone

--- smart-cone-api/src/Audio/audio.service.ts
+++ smart-cone-api/src/Audio/audio.service.ts
@@ -9,23 +9,24 @@ export class AudioService extends BaseAudioService {
     // base path of all audio files
     private basePath = "/var/tmp/ft-audio-files";
     private action = "";
+    private voiceGender = "male";
     // Look up table for action strings, to their filenames as they exist in the base path
     // prettier-ignore
     private actionPaths = {
-        "sprint": `${this.basePath}/sprint.mp3`,
-        "back-pedal": `${this.basePath}/back_pedal.mp3`,
-        "jog": `${this.basePath}/jog.mp3`,
-        "karaoke-left": `${this.basePath}/carioca_left.mp3`,
-        "karaoke-right": `${this.basePath}/carioca_right.mp3`,
-        "side-shuffle-left": `${this.basePath}/side_shuffle_left.mp3`,
-        "side-shuffle-right": `${this.basePath}/side_shuffle_right.mp3`,
-        "butt-kicks": `${this.basePath}/butt_kicks.mp3`,
-        "bounds": `${this.basePath}/bounds.mp3`,
-        "ext-hip-rotation": `${this.basePath}/ext_hip_rotation.mp3`,
-        "high-knees": `${this.basePath}/high_knees.mp3`,
-        "high-skips": `${this.basePath}/high_skips.mp3`,
-        "int-hip-rotation": `${this.basePath}/int_hip_rotation.mp3`,
-        "walking-lunge": `${this.basePath}/walking_lunge.mp3`,
+        "sprint": `${this.basePath}/sprint_${this.voiceGender}.mp3`,
+        "back-pedal": `${this.basePath}/back_pedal_${this.voiceGender}.mp3`,
+        "jog": `${this.basePath}/jog_${this.voiceGender}.mp3`,
+        "karaoke-left": `${this.basePath}/carioca_left_${this.voiceGender}.mp3`,
+        "karaoke-right": `${this.basePath}/carioca_right_${this.voiceGender}.mp3`,
+        "side-shuffle-left": `${this.basePath}/side_shuffle_left_${this.voiceGender}.mp3`,
+        "side-shuffle-right": `${this.basePath}/side_shuffle_right_${this.voiceGender}.mp3`,
+        "butt-kicks": `${this.basePath}/butt_kicks_${this.voiceGender}.mp3`,
+        "bounds": `${this.basePath}/bounds_${this.voiceGender}.mp3`,
+        "ext-hip-rotation": `${this.basePath}/ext_hip_rotation_${this.voiceGender}.mp3`,
+        "high-knees": `${this.basePath}/high_knees_${this.voiceGender}.mp3`,
+        "high-skips": `${this.basePath}/high_skips_${this.voiceGender}.mp3`,
+        "int-hip-rotation": `${this.basePath}/int_hip_rotation_${this.voiceGender}.mp3`,
+        "walking-lunge": `${this.basePath}/walking_lunge_${this.voiceGender}.mp3`,
     };
 
     constructor() {
