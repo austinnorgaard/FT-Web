e6931619e5d6bfe6028c5f223c2c7b47e962b234
Keaton Freude <freudek90@gmail.com>
2020-02-05 04:06:27 -0800

    Fix field-cone install bug

--- packaging/field-cone/install.sh
+++ packaging/field-cone/install.sh
@@ -2,17 +2,22 @@
 
 # Package: field-cone
 
+echo "[field-cone-install]: Killing node..."
 sudo killall node
 
 # This script assumes it ran next to the tar ball
+echo "[field-cone-install]: Unpacking field-cone files"
 tar xf field-cone.tar.gz
 
+echo "[field-cone-install]: Removing existing installation"
 sudo rm -rf /opt/field-cone-api
+
+echo "[field-cone-install]: Installing new installation"
 sudo mv ./field-cone-api /opt/
+
 
 sudo mv ./fieldcone /etc/init.d/fieldcone
 sudo chmod +x /etc/init.d/fieldcone
 sudo update-rc.d fieldcone defaults
 
 sudo systemctl daemon-reload
-sudo systemctl restart fieldcone
