55620485e6af2622d836a8181470baf26031769e
Keaton Freude <freudek90@gmail.com>
2022-01-25 00:36:35 -0800

    angular 10 -> 11

--- field-trainer/field-trainer/src/app/session/components/session-result/session-result.component.spec.ts
+++ field-trainer/field-trainer/src/app/session/components/session-result/session-result.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SessionResultComponent } from './session-result.component';
 
@@ -6,7 +6,7 @@ describe('SessionResultComponent', () => {
   let component: SessionResultComponent;
   let fixture: ComponentFixture<SessionResultComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SessionResultComponent ]
     })
