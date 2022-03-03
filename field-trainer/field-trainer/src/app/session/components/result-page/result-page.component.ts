3afb5d8145faf185902437fdc8f067b89cdc7115
Austin Norgaard <austinnorgaard@gmail.com>
2022-03-03 13:24:23 -0800

    updated results page

--- field-trainer/field-trainer/src/app/session/components/result-page/result-page.component.ts
+++ field-trainer/field-trainer/src/app/session/components/result-page/result-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { SessionService } from "../../services/session.service";
+import { Router } from "@angular/router";
+import { TeamModel } from "../../../teams/models/team";
+import { AthleteModel } from "../../../athletes/models/athlete";
+import { TeamManagementService } from "../../../teams/services/team-management.service";
+import { AthleteManagementService } from "../../../athletes/services/athlete-management.service";
+import { Team } from "../../../../../../../smart-cone-api/src/Teams/team";
 
 @Component({
     selector: "ft-result-page",
@@ -7,13 +12,66 @@ import { SessionService } from "../../services/session.service";
     styleUrls: ["./result-page.component.css"],
 })
 export class ResultPageComponent implements OnInit {
-    constructor(private readonly sessionService: SessionService) {}
+    availableTeams: TeamModel[] = [];
+    selectedTeam: TeamModel = null;
 
-    async onSaveResults() {
-        console.log("Saving results in backend");
-        await this.sessionService.saveResults();
-        console.log("Saved");
+    availableAthletes: AthleteModel[] = [];
+
+    constructor(private teamsService: TeamManagementService, private athletesService: AthleteManagementService) {}
+
+    ngOnInit() {
+        this.updateAthletes();
+        this.updateTeams();
     }
 
-    ngOnInit() {}
+    getTeamDropdownPlaceholderText() {
+        if (this.availableTeams.length === 0) {
+            return "No teams available";
+        } else {
+            return "Select a team";
+        }
+    }
+
+    onTeamChanged() {
+        this.updateAthletes();
+        // Grab the athletes for this team and set them
+    }
+
+    updateAthletes() {
+        this.athletesService
+            .getAthletes()
+            .then((athletes: AthleteModel[]) => {
+                console.log(athletes);
+                this.availableAthletes = athletes;
+            })
+            .catch(err => {
+                console.log(`Could not find any athletes. Err: ${err}`);
+            });
+    }
+
+    updateTeams() {
+        this.teamsService
+            .getTeams()
+            .then((teams: TeamModel[]) => {
+                this.availableTeams = teams;
+            })
+            .catch(err => {
+                console.log(`Could not find any teams. Err: ${err}`);
+            });
+    }
+
+    updateTeamAthletes() {
+        // updates the local athletes list model for the selected team
+        // Ask the server for the team details
+        this.teamsService
+            .getTeamById(this.selectedTeam.id)
+            .then((team: Team) => {
+                // update our local copy of the team with the one we just grabbed
+                this.selectedTeam.teamAthletes = team.teamAthletes;
+            })
+            .catch(err => {
+                console.log(`ERROR: Failed to get team with id: ${this.selectedTeam.id}. This should NOT happen! Err: ${err}`);
+                return;
+            });
+    }
 }
