b45b97dc99f506321544deab95d95132a0cfc21a
Austin Norgaard <austinnorgaard@gmail.com>
2023-04-14 04:04:35 -0700

    updates but more to go

--- scripts/ultrasonic/ultrasonic.py
+++ scripts/ultrasonic/ultrasonic.py
@@ -10,7 +10,7 @@ GPIO.setup(ECHO,GPIO.IN)
 
 GPIO.output(TRIG, False)
 
-print "Starting sensor"
+print ("Starting sensor")
 
 while True:
         GPIO.output(TRIG, True)
@@ -29,6 +29,6 @@ while True:
 
         distance = round(distance, 2)
         if distance < 95:
-                print "Distance:", distance, "cm"
+                print ("Distance:", distance, "cm")
 
 GPIO.cleanup()
